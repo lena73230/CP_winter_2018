befb21c9686d84d44a8177194d9728e5273fcfc9
lenajaslowska <jaslolena@gmail.com>
2018-12-19 00:32:06 +0100

    Customer and account forms

---
+++ src/pl/waw/sgh/bank/ChangeAccountsUI.java
@@ -0,0 +1,13 @@
+package pl.waw.sgh.bank;
+
+import javax.swing.*;
+
+public class ChangeAccountsUI {
+
+    private JTextField textField1;
+    private JTextField textField2;
+    private JTextField textField3;
+    private JButton saveChangesButton;
+    private JButton seeCustomerButton;
+    private JButton cancelButton;
+}
---
+++ src/pl/waw/sgh/bank/ChangeCustomerUI.java
@@ -0,0 +1,13 @@
+package pl.waw.sgh.bank;
+
+import javax.swing.*;
+
+public class ChangeCustomerUI {
+    private JTabbedPane tabbedPane1;
+    private JTextField textField1;
+    private JTextField textField2;
+    private JButton saveChangesButton;
+    private JButton editAccountsButton;
+    private JButton cancelButton;
+    private JTextField textField3;
+}
